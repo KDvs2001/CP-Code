84eaacb771cc9df42aefd24a30569b39d74284af
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2025-10-22 09:48:36 +0530

    Updated Concurrent Week4 (New Folder contains PartA and B as a combined + StateMachine Diagram PDF)

---
+++ Concurrent_Week4/Concurrent_Week4_Part1_SharedBankAcc/src/main/java/org/example/HBH.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.math.BigDecimal;
+
+public class HBH implements Runnable{
+    private BankAccount bankAccount;
+
+    public HBH(BankAccount bankAccount){
+        this.bankAccount = bankAccount;
+    }
+    @Override
+    public void run(){
+        bankAccount.withdraw(BigDecimal.valueOf(20000));
+    }
+}
